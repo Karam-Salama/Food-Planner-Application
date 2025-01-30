367e81b81c3bf6a21b4890d1afe23e5835ce0428
Karam Salama <karamslama244@gmail.com>
2025-01-30 21:27:29 +0200

    - added splash screen - added logic of splash (delayed nav to onboarding screen) - hide status bar & navigation bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.foodplannerapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.foodplannerapplication"
