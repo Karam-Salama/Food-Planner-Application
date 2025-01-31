0284bc06d1ab7dd87002f956ee41d2204ef40654
Karam Salama <karamslama244@gmail.com>
2025-02-01 01:17:09 +0200

    - hide status bar & navigation bar of splash screen - create onboarding screen & finisg ui - add fonts & imgs with different densities - build selector with two states of onboarding_btn (pressed or not) - add some strings and colors - add lottie dependency to my app - add lottie view in the splash_activity and add text_view

--- app/src/main/java/com/example/foodplannerapplication/modules/splash/SplashActivity.kt
+++ app/src/main/java/com/example/foodplannerapplication/modules/splash/SplashActivity.kt
@@ -1,14 +1,19 @@
-package com.example.foodplannerapplication
+package com.example.foodplannerapplication.modules.splash
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodplannerapplication.modules.onboarding.OnboardingActivity
+import com.example.foodplannerapplication.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +26,23 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        // hide status bar and navigation bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.systemBars())
+        } else {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+
+
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, OnboardingActivity::class.java))
             finish()
         }, 3000)
-
     }
 }
