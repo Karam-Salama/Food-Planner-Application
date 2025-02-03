8abef1d064e09c2afa0cf6cf3ff99c650db026ca
Karam Salama <karamslama244@gmail.com>
2025-02-04 01:28:43 +0200

    added ForgetPassword Activity added google image with different densities

--- app/src/main/java/com/example/foodplannerapplication/modules/auth/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/foodplannerapplication/modules/auth/ForgetPasswordActivity.kt
@@ -1,24 +1,17 @@
 package com.example.foodplannerapplication.modules.auth
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.foodplannerapplication.R
 
-class RegisterActivity : AppCompatActivity() {
-    private lateinit var tv_login:TextView
+class ForgetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_forget_password)
 
-        tv_login = findViewById(R.id.tv_login)
-        tv_login.setOnClickListener {
-            finish()
-        }
     }
 }
